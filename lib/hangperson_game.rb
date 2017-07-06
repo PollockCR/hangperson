bd4c2d6541aca220ba0dcc1bd1c067faf8bfd6e7
Catherine Pollock <pollockcr@gmail.com>
2017-07-06 01:37:23 +0000

    Finish part 2 of hangperson assignment

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,11 +5,59 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    raise ArgumentError, 'Please guess a letter' unless
+      letter != '' && letter != nil && letter.match(/[a-z]/i)
+    letter.downcase!
+    if @word.include?(letter)
+      if !@guesses.include?(letter)
+        @guesses += letter
+      else
+        return false
+      end
+    else
+      if !@wrong_guesses.include?(letter)
+        @wrong_guesses += letter
+      else
+        return false
+      end
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    display = ''
+    @word.chars.each do |x| 
+      if @guesses.include? x 
+        display += x
+      else
+        display += '-'
+      end
+    end
+    return display
+  end
+  
+  def check_win_or_lose
+    @word.chars.each do |x|
+      if !@guesses.include? x 
+        if @wrong_guesses.size == 7
+          return :lose
+        else
+          return :play
+        end
+      end
+    end
+    return :win
   end
 
   def self.get_random_word
